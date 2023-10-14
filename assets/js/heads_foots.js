6c404cb7545e219ffc8e5289dc428860ee1b9e51
eugeniocachiombo <eugeniocachiombo@gmail.com>
2023-10-14 14:50:00 +0100

    Reorganizando tudo novamente

---
+++ assets/js/heads_foots.js
@@ -0,0 +1,5 @@
+$("#headHTML").load("../../../app/views/inc/headHTML.html");
+$("#header").load("../../../app/views/inc/header.html");
+$("#paginaInicial").load("../../../app/views/pagina_inicial/estrutura.html");
+$("#footer").load("../../../app/views/inc/footer.html");
+$("#footHTML").load("../../../app/views/inc/footHTML.html");
